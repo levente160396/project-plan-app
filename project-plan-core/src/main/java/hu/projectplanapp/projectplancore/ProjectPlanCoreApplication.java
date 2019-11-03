780c967ea6903ef69f5e29351d915dbeccb07583
szabó levente <levente160396@gmail.com>
2019-11-03 21:30:48 +0100

    Add core to project

---
+++ project-plan-core/src/main/java/hu/projectplanapp/projectplancore/ProjectPlanCoreApplication.java
@@ -0,0 +1,13 @@
+package hu.projectplanapp.projectplancore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPlanCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectPlanCoreApplication.class, args);
+	}
+
+}
---
+++ project-plan-core/src/test/java/hu/projectplanapp/projectplancore/ProjectPlanCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.projectplanapp.projectplancore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPlanCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
